2a18e41fe18e7e1b1a3f732a8aa301388c94d78a
Stephen Nayfach <snayfach@gmail.com>
2014-11-11 17:42:09 -0800

    added MANIFEST.in, setup.py and updated README

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'MicrobeCensus',
+	
+	version = '1.2.1',
+	
+	description = 'A command-line tool for estimating average genome size from shotgun sequence data',
+	
+	install_requires=['numpy', 'biopython'],
+	
+	license = 'GPL',
+	
+	url = 'https://github.com/snayfach/MicrobeCensus',
+	
+	author = 'Stephen Nayfach',
+    
+	author_email='snayfach@gmail.com',
+	
+	classifiers = [
+		'Programming Language :: Python',
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+		'Development Status :: 5 - Production/Stable',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Unix',
+		'Intended Audience :: Science/Research',
+		'Topic :: Scientific/Engineering :: Bio-Informatics'],
+
+	keywords='MicrobeCensus metagenomics genomics microbiology ecology'
+)

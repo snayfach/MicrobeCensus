6bd195db98217f62bd44933f893d64957f47aa6d
snayfach <snayfach@gmail.com>
2015-01-28 20:37:17 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'MicrobeCensus',
-	
 	version = '1.2.1',
-	
 	description = 'A command-line tool for estimating average genome size from shotgun sequence data',
-	
 	install_requires=['numpy', 'biopython'],
-	
 	license = 'GPL',
-	
 	url = 'https://github.com/snayfach/MicrobeCensus',
-	
 	author = 'Stephen Nayfach',
-    
 	author_email='snayfach@gmail.com',
-	
 	classifiers = [
 		'Programming Language :: Python',
 		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
@@ -26,5 +18,6 @@ setup(
 		'Intended Audience :: Science/Research',
 		'Topic :: Scientific/Engineering :: Bio-Informatics'],
 
-	keywords='MicrobeCensus metagenomics genomics microbiology ecology'
+	keywords='MicrobeCensus metagenomics genomics microbiology ecology',
+	scripts=['src/microbe_census.py'],
 )

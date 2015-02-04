9645731185fef9c6b7b365adac5c117c1306fe47
Stephen Nayfach <snayfach@gmail.com>
2015-02-03 18:42:03 -0800

    setup

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ try: from setuptools import setup
 except: from distutils.core import setup
 
 setup(
-	name = 'test123098',
-	version = '1.0.4',
-	packages = ['microbe_census'],
-	package_data={'microbe_census': ['data/*', 'bin/*']},
+	name = 'MicrobeCensus',
+	version = '1.3.0',
+	packages = ['microbe_census', 'training', 'tests'],
+	package_data={
+		'microbe_census': ['data/*', 'bin/*', 'example/*'],
+		#'training': ['example/*'],
+		'tests': ['data/*']},
 	scripts=['scripts/run_microbe_census.py'],
 	license = 'GPL',
 	author = 'Stephen Nayfach',
 	author_email='snayfach@gmail.com',
-	url='https://github.com/snayfach/test',
+	url='https://github.com/snayfach/MicrobeCensus',
 	install_requires = ['biopython', 'numpy']
 )

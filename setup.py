dcc93ae13c9f923c799717acf52fc9184298e3fe
Stephen Nayfach <snayfach@gmail.com>
2015-02-04 13:44:02 -0800

    updated version no

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class my_install_lib(install_lib.install_lib):
 
 setup(
 	name = 'MicrobeCensus',
-	version = '1.0',
+	version = '1.0.1',
 	description = 'Estimation of average genome size from metagenomic data',
 	packages = ['microbe_census', 'tests'],
 	package_data={
